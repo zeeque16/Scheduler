48caf7b21cef3ad935f0aa608c47b17fbbfc9c3b
zeeque16 <48221528+zeeque16@users.noreply.github.com>
2019-09-27 12:58:04 +0500

    Today's date is taken from system and stored

--- MedScheduler.java
+++ MedScheduler.java
@@ -1,17 +1,30 @@
 import java.util.Scanner;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class MedScheduler {
+
+	private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
     public static void main(String[] args) {
+
+     	LocalDate localDate = LocalDate.now();
+     	System.out.println(localDate);
+
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter date: ");
+        System.out.println("Enter number of days medicine to be taken: ");
 
         int date = myObj.nextInt();
+
+        myObj = new Scanner(System.in);
+        System.out.println("Enter the gap between days medicine needs to be taken: ");
+        myObj.close();
 
         date = dateFunction(date);
         System.out.println("Date: " + date);
 
-
-        myObj.close();
     }
 
     public static int dateFunction(int date) {
